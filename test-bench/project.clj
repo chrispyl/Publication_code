41077474283d224ce30984223d2e4739fc182cd9
Christos Pylianidis <pylianidis@gmail.com>
2017-09-05 18:58:30 +0300

    updated infix dependency

--- test-bench/project.clj
+++ test-bench/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-				[rm-hull/infix "0.2.10"]
+				[rm-hull/infix "0.3.0"]
 				[criterium "0.4.4"]
 				[org.clojure/math.combinatorics "0.1.4"]
 				[funcool/promissum "0.3.3"]
